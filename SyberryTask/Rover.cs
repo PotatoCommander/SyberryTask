bee1ee8e3ebd74d032a34a515389b116a14ee16f
Nikita Bodry <bodrik66aa@gmail.com>
2021-05-12 23:19:15 +0300

    Created base classes for implementation of A* algo

--- SyberryTask/Rover.cs
+++ SyberryTask/Rover.cs
@@ -1,6 +1,39 @@
-﻿public class Rover {
+﻿using System;
+using System.Collections.Generic;
 
-    public static void CalculateRoverPath(int[,] map) {
+public class Rover 
+{
+    public class LatticeGraph
+    {
+        public int[,] weights;
+        
+        public int width;
+        public int height;
+
+        //returns: weight of edge between two neighbour vertexes in graph
+        //TODO: make sure that this method calls only for neighbours
+        public int GetCost(VertexPlace a, VertexPlace b)
+        {
+            var x1 = a.X;
+            var y1 = a.Y;
+            var x2 = b.X;
+            var y2 = b.Y;
+            return Math.Abs(weights[x1, y1] - weights[x2, y2]) + 1;
+        }
+    }
+    public class VertexPlace
+    {
+        public int X;
+        public int Y;
+        public VertexPlace(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
     }
 
+    public static void CalculateRoverPath(int[,] map)
+    {
+    }
+    
 }
